97df330630e122df4e1b9db65ba093768bf6aaa8
Martin Slavsky <martinboca22@yahoo.com.ar>
2023-05-29 22:57:38 -0300

    routes

--- martin-react/src/App.jsx
+++ martin-react/src/App.jsx
@@ -2,13 +2,18 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ItemListContainer from './components/ItemListContainer';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="container-fluid">
+      <BrowserRouter>
       <Header />
-      <ItemListContainer />
+      <Routes>
+          <Route path={"/"} element={<ItemListContainer />} />
+      </Routes>
       <Footer />
+      </BrowserRouter>
     </div>
   );
 }
